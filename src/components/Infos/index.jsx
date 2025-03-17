b68b77d7f214f0a55fe8e956c6b7e1a97ee6d6ac
dudaPetracek <157414482+dudaPetracek@users.noreply.github.com>
2025-03-17 09:41:03 -0300

    Add files via upload

--- src/components/Infos/index.jsx
+++ src/components/Infos/index.jsx
@@ -1,40 +1,41 @@
 const Infos = () => {
     return (
-    <table className="table caption-top">
+      <table className="table caption-top">
         <caption>Como Interpretar o resultado de IMC</caption>
         <thead>
-        <tr>
+          <tr>
             <th scope="col">IMC</th>
             <th scope="col">Classificação</th>
-        </tr>
+          </tr>
         </thead>
         <tbody>
-        <tr>
+          <tr>
             <th scope="row">Menor que 18,5</th>
             <td>Magreza</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th scope="row">18,5 a 24,9</th>
             <td>Normal</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th scope="row">25 a 29,9</th>
             <td>SobrePeso</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th scope="row">30 a 34,9</th>
             <td>Obesidade grau I </td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th scope="row">35 a 39,9</th>
             <td>Obesidade grau II</td>
-        </tr>
-        <tr>
+          </tr>
+          <tr>
             <th scope="row">Maior que 40</th>
             <td>Obesidade grau III</td>
-        </tr>
+          </tr>
         </tbody>
-    </table>
+      </table>
     );
-};
-export default Infos;
+  };
+  
+  export default Infos;
